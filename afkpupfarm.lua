a0032df6a3ae8cca9d3d7f8208b06ab4e58cbdfe
Alvin Dimas <74313216+alvindimas05@users.noreply.github.com>
2022-04-20 20:14:23 +0700

    AFK Gold/Diamond Pup Farm
    AFK Gold/Diamond Pup Farm

---
+++ afkpupfarm.lua
@@ -0,0 +1,29 @@
+local Players = game:GetService("Players")
+local Workspace = game:GetService("Workspace")
+local Leg
+
+local Blackout = game.Workspace.Events:FindFirstChild("Blackout")
+local Power = game.Workspace.Events:FindFirstChild("PowerOutage")
+local Player = Players.LocalPlayer
+local Touch = Workspace.Scripted.TransformBrick.SlimePup:FindFirstChild("TouchInterest", true).Parent
+while true do
+    Player.Character.HumanoidRootPart.CFrame = CFrame.new(37, 1, -310)
+    wait(1)
+    for i = 1, 6 do
+        firetouchinterest(Player.Character.HumanoidRootPart, Touch, 0)
+        firetouchinterest(Player.Character.HumanoidRootPart, Touch, 1)
+        wait(1.1)
+    end
+    wait(8)
+    local Leg = Player.Character:FindFirstChild("Left Leg")
+    if Leg.BrickColor == BrickColor.new("Bright yellow") or Leg:FindFirstChild("ShinyParticle") then
+        print("Congrats!")
+        break
+    elseif Blackout.Value == true or Power.Value == true then
+        print("Blackout or Power Outage is ON!")
+        break
+    else
+        Player.Character.Humanoid.Health = 0
+        wait(17)
+    end
+end
